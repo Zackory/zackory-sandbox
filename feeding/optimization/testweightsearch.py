c7e43b024f155167cb5f57a93f5a26c5b02767eb
Zackory <zackory@gatech.edu>
2016-08-02 18:31:29 -0400

    Beginning to clean up weight search.

--- feeding/optimization/testweightsearch.py
+++ feeding/optimization/testweightsearch.py
@@ -1,86 +1,6 @@
-import os, sys, rospy
-import numpy as np
-import cPickle as pickle
-from collections import deque
-from joblib import Parallel, delayed
-from sklearn import preprocessing, cross_validation
-from sklearn.grid_search import GridSearchCV
+import weightsearch
 
-import hrl_lib.util as ut
-from hrl_anomaly_detection import util
-from hrl_anomaly_detection import data_manager as dm
-from hrl_anomaly_detection.ICRA2017_params import *
-
-# learning
-from hrl_anomaly_detection.hmm import learning_hmm
-from hrl_anomaly_detection.hmm import learning_util as hmm_util
-
-# Classifier
-from hrl_anomaly_detection.classifiers import classifier as clf
-from hrl_anomaly_detection.classifiers.classifier_util import *
-
-
-
-class WeightSearch:
-    def __init__(self, classifier, evalFunction):
-        self.classifier = classifier
-        self.evalFunction = evalFunction
-
-    def findBestWeight(self, weightsList, unflattenedX, unflattenedY, itersPerWeight=10):
-        '''
-        Returns the best weight from a provided weights list given a specific classifier, evaluation function, and unflattened data
-        '''
-        # Determine which data are anomalous and nonanomalous
-        nonAnomalousX = []
-        nonAnomalousY = []
-        anomalousX = []
-        anomalousY = []
-        for X, y in zip(unflattenedX, unflattenedY):
-            if y[0] < 0:
-                nonAnomalousX.append(X)
-                nonAnomalousY.append(y)
-            else:
-                anomalousX.append(X)
-                anomalousY.append(y)
-
-        # Flatten data and train a Standard Scaler
-        flatX = np.reshape(unflattenedX, (-1, np.shape(unflattenedX)[-1]))
-        scaler = preprocessing.StandardScaler()
-        scaler.fit(flatX)
-
-        weightAccuracy = []
-        # Try every weight
-        for weight in weightsList:
-            # Set class weight for classifier
-            self.classifier.set_params(class_weight=weight)
-            # Create itersPerWeight randomized splits while maintaining an even balance of anomalous and nonanomalous data in each training set and validation set
-            split = cross_validation.StratifiedShuffleSplit([1]*len(nonAnomalousX) + [-1]*len(anomalousX), n_iter=itersPerWeight, test_size=0.4)
-            accuracies = []
-            for trainIdx, valIdx in split:
-                # Set up training and validation splits
-                trainX, trainY = unflattenedX[trainIdx], unflattenedY[trainIdx]
-                valX, valY = unflattenedX[valIdx], unflattenedY[valIdx]
-
-                # Flatten and scale data, then fit classifier
-                trainX = np.reshape(trainX, (-1, np.shape(trainX)[-1]))
-                trainY = np.reshape(trainY, -1)
-                self.classifier.fit(scaler.transform(trainX), trainY)
-                # Scale validation data and calculate accuracy
-                for i in xrange(len(valX)):
-                    valX[i] = self.scaler.transform(valX[i])
-                results = self.evalFunction(valX, valY, self.classifier)
-                if type(results) is list or type(results) is tuple:
-                    accuracies.append(results[0])
-                else:
-                    accuracies.append(results)
-            weightAccuracy.append(np.mean(accuracies))
-            print 'Mean accuracy for weight %f:' % weight, np.mean(accuracies)
-        bestWeightIndex = np.argmax(weightAccuracy)
-        bestWeight = weightsList[bestWeightIndex]
-
-        return bestWeight, weightAccuracy[bestWeightIndex]
-
-
+class TestWeightSearch:
     def __init__(self):
         self.initParams()
         # Load HMM and Training/Test Data
@@ -408,8 +328,4 @@ def evaluation(clf, X, Y, verbose=False):
     # return acc, np.sum(fp_l), np.sum(fn_l)
 
 
-
-
-if __name__ == '__main__':
-    searcher = WeightSearch()
 
